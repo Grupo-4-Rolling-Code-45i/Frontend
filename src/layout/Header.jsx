2e16af3a9fbc872b467e7a9afd156f0a41e697bf
Nicolas Quinteros <nicoquinteros.nq@gmail.com>
2023-07-25 22:41:45 -0300

    agregue logo de pizzeria

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -3,16 +3,16 @@ export default function header(){
     return <>
    <header>
       
-        <nav class="navbar">
-          <a  href="">  <img class="logo-navbar" src="src\assets\logo-pizzeria.png" alt="" /></a>
+        <nav className="navbar">
+          <a  href="">  <img className="logo-navbar" src="src\assets\logo-pizzeria.png" alt="" /></a>
         
   
-          <ul class="lista-navbar">
+          <ul className="lista-navbar">
             <li><a href="">Inicio</a></li>
             <li><a href="">Nuestros productos</a></li>
             <li><a href="">Sobre nosotros</a></li>
             <li><a href="">Contacto</a></li>
-            <li><a href=""> <img class="logo-carrito-navbar" src="src\assets\Carrito.png" alt="" /> </a></li>
+            <li><a href=""> <img className="logo-carrito-navbar" src="src\assets\Carrito.png" alt="" /> </a></li>
           </ul>
         </nav>
       </header>
