a256376e4e4326804ae37492971cfff079f0fdd4
Nicolas Quinteros <nicoquinteros.nq@gmail.com>
2023-07-25 21:43:19 -0300

    esqueleto del navbar

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,6 +1,21 @@
 export default function header(){
 
     return <>
-   <p>INSTALACION DE DEPENDENCIAS COMPLETA</p>
-    </>
+   <header>
+      
+        <nav class="navbar">
+          <a class="logo-navbar" href="">  <div>LOGO</div> </a>
+        
+  
+          <ul class="lista-navbar">
+            <li><a href="">Inicio</a></li>
+            <li><a href="">Nuestros productos</a></li>
+            <li><a href="">Sobre nosotros</a></li>
+            <li><a href="">Contacto</a></li>
+            <li><a href="">carrito</a></li>
+          </ul>
+        </nav>
+      </header>
+   
+   </>
 }
