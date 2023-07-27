588057082c680f6e1e0fa86bbd0b6c0150e01e46
Máximo Majorel <maximomajorel@gmail.com>
2023-07-27 10:57:41 -0300

    Maqueta sobre nosotros Desktop

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './layout/Header'
 import { HomePage } from './home/pages/HomePage';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Aboutus } from './about-us/pages/AboutUs';
 
 export default function App() {
   return (
@@ -10,7 +11,7 @@ export default function App() {
       <BrowserRouter>
         <Routes>
             <Route path='/' element={ <HomePage/>}/>
-
+            <Route path='/sobre-nosotros' element={ <Aboutus/>}/>
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/about-us/pages/AboutUs.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import '../css/aboutus.css'
+export const Aboutus = () => {
+  return (
+    <>
+    <main className='sobreNosotros-main-container'>
+      <div className='sobreNosotros-titulo-container'>
+      <h2 className='sobreNosotros-titulo'>Sobre nosotros</h2>
+      <h3 className='sobreNosotros-Texto-intro'>Descubre quienes están detras de este proyecto...</h3>
+      </div>
+    <div className='sobreNosotros-container-cards'>
+      <div className='sobreNosotros-card'>
+        <div className='sobreNosotros-image-container'>
+          <img src="" alt="" />
+        </div>
+        <h4 className='sobreNosotros-nombre-card'>Nombre 1</h4>
+        <p className='sobreNosotros-desc-card'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis quis blanditiis sint error quos doloribus saepe temporibus facilis beatae vero praesentium cupiditate, animi aspernatur eius ipsum repellendus illo velit dolores.</p>
+        <div className="sobreNosotros-conteiner-botones">
+          <button className='sobreNosotros-boton-social'><i class="fa-brands fa-linkedin-in"></i></button>
+          <button className='sobreNosotros-boton-social'><i class="fa-brands fa-github"></i></button>
+        </div>
+      </div>
+      <div className='sobreNosotros-card'>
+        <div className='sobreNosotros-image-container'>
+          <img src="" alt="" />
+        </div>
+        <h4 className='sobreNosotros-nombre-card'>Nombre 1</h4>
+        <p className='sobreNosotros-desc-card'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis quis blanditiis sint error quos doloribus saepe temporibus facilis beatae vero praesentium cupiditate, animi aspernatur eius ipsum repellendus illo velit dolores.</p>
+        <div className="sobreNosotros-conteiner-botones">
+          <button className='sobreNosotros-boton-social'><i class="fa-brands fa-linkedin-in"></i></button>
+          <button className='sobreNosotros-boton-social'><i class="fa-brands fa-github"></i></button>
+        </div>
+      </div>
+      <div className='sobreNosotros-card'>
+        <div className='sobreNosotros-image-container'>
+          <img src="" alt="" />
+        </div>
+        <h4 className='sobreNosotros-nombre-card'>Nombre 1</h4>
+        <p className='sobreNosotros-desc-card'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis quis blanditiis sint error quos doloribus saepe temporibus facilis beatae vero praesentium cupiditate, animi aspernatur eius ipsum repellendus illo velit dolores.</p>
+        <div className="sobreNosotros-conteiner-botones">
+          <button className='sobreNosotros-boton-social'><i class="fa-brands fa-linkedin-in"></i></button>
+          <button className='sobreNosotros-boton-social'><i class="fa-brands fa-github"></i></button>
+        </div>
+      </div>
+      <div className='sobreNosotros-card'>
+        <div className='sobreNosotros-image-container'>
+          <img src="" alt="" />
+        </div>
+        <h4 className='sobreNosotros-nombre-card'>Nombre 1</h4>
+        <p className='sobreNosotros-desc-card'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis quis blanditiis sint error quos doloribus saepe temporibus facilis beatae vero praesentium cupiditate, animi aspernatur eius ipsum repellendus illo velit dolores.</p>
+        <div className="sobreNosotros-conteiner-botones">
+          <button className='sobreNosotros-boton-social'><i class="fa-brands fa-linkedin-in"></i></button>
+          <button className='sobreNosotros-boton-social'><i class="fa-brands fa-github"></i></button>
+        </div>
+      </div>
+    </div>
+    </main>
+    </>
+  )
+}
