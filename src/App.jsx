12f89ea093cc33ac2f0edee179ed9832d6909e15
Máximo Majorel <maximomajorel@gmail.com>
2023-07-27 16:19:25 -0300

    Informacion de participantes en el about us, botones vinculados en la home

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './layout/Header'
 import { HomePage } from './home/pages/HomePage';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Error404 } from './404/pages/Error404';
 import { Aboutus } from './about-us/pages/AboutUs';
 
 export default function App() {
@@ -11,6 +12,7 @@ export default function App() {
       <BrowserRouter>
         <Routes>
             <Route path='/' element={ <HomePage/>}/>
+            <Route path='*' element={ <Error404/>}/>            
             <Route path='/sobre-nosotros' element={ <Aboutus/>}/>
         </Routes>
       </BrowserRouter>
