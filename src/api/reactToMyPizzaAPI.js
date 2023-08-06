dcebcb9daeffedae38971e5bc9e0d375c5e0c0d2
damianpazv <damjpv@gmail.com>
2023-08-06 10:24:56 -0300

    se dejo comentado el header del token

--- src/api/reactToMyPizzaAPI.js
+++ src/api/reactToMyPizzaAPI.js
@@ -4,4 +4,11 @@ const reactToMyPizzaAPI = axios.create({
   baseURL: "http://localhost:4500/",
 });
 
+
+// reactToMyPizzaAPI.interceptors.request.use((config)=>{
+//     config.headers={ "x-token": localStorage.getItem("token"),   };
+//     return config;
+// })
+
+
 export default reactToMyPizzaAPI;
