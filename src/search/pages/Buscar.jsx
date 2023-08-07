28924c7937e84150816853294511cb3c6cd2862a
Máximo Majorel <maximomajorel@gmail.com>
2023-08-07 20:57:30 -0300

    Fix pantalla en blanco cuando no se encontraban resultados

--- src/search/pages/Buscar.jsx
+++ src/search/pages/Buscar.jsx
@@ -1,34 +1,33 @@
 import React, { useEffect, useState } from 'react';
 import reactToMyPizzaAPI from '../../api/ReactToMyPizzaAPI';
 import { Link } from 'react-router-dom';
-import '../css/busqueda.css'
+import '../css/busqueda.css';
+
 export const Buscar = () => {
   const urlParamsBuscar = new URLSearchParams(window.location.search);
   const termino = urlParamsBuscar.get('buscar');
   const [productosEncontrados, setProductosEncontrados] = useState([]);
 
   useEffect(() => {
-    try {
-      reactToMyPizzaAPI.get(`api/products/buscar/${termino}`).then((response) =>{
+    reactToMyPizzaAPI.get(`api/products/buscar/${termino}`)
+      .then((response) => {
         const respProductosEncontrados = response.data.response;
         setProductosEncontrados(respProductosEncontrados);
-        console.log(respProductosEncontrados); 
+      })
+      .catch((error) => {
+        console.log('Ha ocurrido un error al obtener los productos encontrados:', error);
       });
-    } catch (error) {
-      console.log('Ha ocurrido un error al obtener los productos encontrados:', error);
-    }
   }, [termino]);
 
   return (
-    <>
     <section className='resultados-de-busqueda-section'>
       <h2 className='home-tercer-section-tituloMenu'>Resultados de la búsqueda: </h2>
-      {productosEncontrados.length === 0 ? (
-        <p>No se encontraron productos.</p>
-      ) : (
-        <div className='home-contenedor-cards-productos'>
-          {productosEncontrados.map((producto) => (
-            <div key={producto.id} className="home-card-productos">
+      <div className='home-contenedor-cards-productos'>
+        {(productosEncontrados === undefined) ? (
+          <p>No se encontraron productos.</p>
+        ) : (
+          productosEncontrados.map((producto) => (
+            <div key={producto._id} className="home-card-productos">
               <img src={producto.imagen} alt={producto.nombre} className="home-imagen-productos"/>
               <div className='home-contenedor-info-productos'>
                 <h2 className='home-nombre-producto'>{producto.nombre}</h2>
@@ -39,10 +38,9 @@ export const Buscar = () => {
                 </Link>
               </div>
             </div>
-          ))}
-        </div>
-      )}
-      </section>
-    </>
+          ))
+        )}
+      </div>
+    </section>
   );
-}
+};
