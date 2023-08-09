3ea22cc32f7ff969ad1165a48727267fab40fddf
José Agustín Rubio <rubioagustin24@gmail.com>
2023-08-09 18:43:44 -0300

    feature/productos (#51)
    Agregadas confirmaciones al apretar el boton de realizar pedido.

--- src/products/pages/products.jsx
+++ src/products/pages/products.jsx
@@ -3,29 +3,32 @@ import { Button , Container } from 'react-bootstrap';
 import '../css/products.css';
 import { Route } from "react-router";
 
+import Swal from "sweetalert2";
+
 import { PizzeriaContext } from "../../PedidosContext/PedidosContext";
 
 import { Link } from "react-router-dom";
 import reactToMyPizzaAPI from "../../api/ApiReactToMyPizza";
 export const Products = () => {
     const urlParams  = new URLSearchParams(window.location.search);
-    const id = urlParams.get('id');  
+    const id = urlParams.get('id'); 
     const [producto, setProducto] = useState([])
-    
+
     useEffect(() => {
-      try {
+    try {
         reactToMyPizzaAPI.get(`api/products/get-one/${id}`).then((response) =>{
             const RespProducto = response.data.response;
             console.log(RespProducto)
-            
+
             setProducto(RespProducto)
-            
+
         })
-      } catch (error) {
+    } catch (error) {
         console.log("Ocurrió un error, por favor contactese con el administrador")
-      }    
-      
+    }
+
     }, [])
+
     const {nombre,precio,descripcion,imagen} = producto
 
     // Mostrar todos los productos:
@@ -33,11 +36,11 @@ export const Products = () => {
         useEffect(() => {
             try {
             // Peticion GET (Todos los productos)
-          reactToMyPizzaAPI.get('/api/products/').then((response) =>{
-            const listaProductos = response.data.response;            
+        reactToMyPizzaAPI.get('/api/products/').then((response) =>{
+            const listaProductos = response.data.response;
             setProductos(listaProductos)
-            
-          })    
+
+            })
         } catch (error) {
                 console.log(`Ha ocurrido un error a la hora de querer obtener los productos, por favor contacte un administrador: ${error}`)
             }
@@ -60,27 +63,32 @@ export const Products = () => {
             if(currentUser) {
 
                 const usuarioId4 = currentUser._id;
-
-                
-                  const  nombre = producto.nombre
-                  const  precio = producto.precio
-                  const  cantidad = parseInt(cantidadAEnviar, 10)
-                 const    usuario = usuarioId4
-                
+                const  nombre = producto.nombre
+                const  precio = producto.precio
+                const  cantidad = parseInt(cantidadAEnviar, 10)
+                const   usuario = usuarioId4
 
                 try{
                     await reactToMyPizzaAPI.post('/api/cart/new',{ nombre, precio, cantidad, usuario})
-                       // console.log(bodyIDadd)
-
+                    Swal.fire({
+                        icon: "success",
+                        title: "Listo!",
+                        text: "Pedido agregado al carrito",
+                        confirmButtonText: 'OK',
+                    })
                 }
 
-                
-                 
+
                     catch(error)  {
                         console.error('Error al agregar el producto:', error);
-                        console.log(bodyIDadd);
-                        console.log(error);
-                    };
+                    }
+            } else {
+                Swal.fire({
+                    icon: "info",
+                    title: "Importante",
+                    text: "Para agregar al pedido debes iniciar sesión.",
+                    confirmButtonText: 'OK',
+                });
             }
         };
 
