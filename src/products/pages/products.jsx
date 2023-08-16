64389778f3f6c20793257917f26e5d183baf4a55
Máximo Majorel <maximomajorel@gmail.com>
2023-08-16 14:39:34 -0300

    Fix disposicion de elementos en todas las cards de productos

--- src/products/pages/products.jsx
+++ src/products/pages/products.jsx
@@ -153,9 +153,13 @@ export const Products = () => {
             <div key={producto.id} className="home-card-productos">
                 <img src={producto.imagen} alt={producto.nombre} className="home-imagen-productos"/>
                 <div className='home-contenedor-info-productos'>
-                <h2 className='home-nombre-producto'>{producto.nombre}</h2>
+                <h2 className='home-nombre-producto'>{producto.nombre}</h2>                
                 <p className='home-precio-producto'>${producto.precio}</p>
-                <p className='home-descripcion-producto'>{producto.descripcion}</p>
+                <div className='home-descripcion-producto'>
+                <p className='contenido-parrafo'>{producto.descripcion}</p>
+
+                </div>
+                
                 <Link onClick={() => Window.location.reload()} to={`/producto?id=${producto._id}`}>
                 <button className='home-boton-verProducto'>Ver producto</button>
                 </Link>
